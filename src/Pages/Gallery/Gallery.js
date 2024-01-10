385965059f000b9712e8f2cdfbff788963777c38
alexflores1231 <alexanderflores_10_@hotmail.es>
2024-01-09 22:06:44 -0300

    a

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -3,7 +3,11 @@ import "./Gallery.scss";
 import { icons } from "../../Data/Icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function Gallery() {
-
+    return (
+        <div className="col-sm-12 gallery">
+          
+        </div>
+      );
 
 }
 
