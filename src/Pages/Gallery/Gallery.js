898b3c568c67db15f6077c2ed3f962927ab71ca0
alexflores1231 <alexanderflores_10_@hotmail.es>
2024-01-09 23:11:03 -0300

    Se agrego el carousel, falta agregar los botones de "ver mas".

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -8,13 +8,38 @@ import foto3 from "../../assets/Gallery/foto3.png"
 import foto4 from "../../assets/Gallery/foto4.png"
 import foto5 from "../../assets/Gallery/foto5.png"
 import foto6 from "../../assets/Gallery/foto6.png"
-
+import carousel from "../../assets/Gallery/carusel.png"
 function Gallery() {
     return (
         <div className="col-sm-12 gallery">
+          {/* Aqui va el carousel  */}
           <div className="col-sm-5 caja">
-
+            <div id="demo" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+                <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+                <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src={carousel} alt="Los Angeles" class="d-block w-100"/>
+                </div>
+                <div class="carousel-item">
+                  <img src={foto1} alt="Chicago" class="d-block w-100"/>
+                </div>
+                <div class="carousel-item">
+                  <img src={foto2} alt="New York" class="d-block w-100"/>
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+                <span class="carousel-control-next-icon"></span>
+              </button>
+            </div>
           </div>
+          {/* Aqui va la tabla de 6 fotos   */}
           <div className="col-sm-7 caja1">
             <div className="col-sm-12 caja1-1">
               <div className="col-sm 4 caja1-1-1" style={{
