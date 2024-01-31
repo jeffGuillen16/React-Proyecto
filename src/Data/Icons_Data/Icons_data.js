a64efdc5696255f0790848eacc6253d0a3eb0615
Daniela Fernandez Laura <dfernandezl@fi.uba.ar>
2024-01-31 17:26:35 -0300

    Creating an array of objects

---
+++ src/Data/Icons_Data/Icons_data.js
@@ -0,0 +1,27 @@
+import icon1 from "../../assets/Hero/Eco-Friendly-Icon.svg";
+import icon2 from "../../assets/Hero/Payment-Methods-Icon.svg";
+import icon3 from "../../assets/Hero/Screen-Printing-Icon.svg";
+
+export const iconsWithText = [
+    {
+        "id": 1,
+        "title": "ECO-FRIENDLY",
+        "content": "Nuestros productos son 100% amigables con el medio ambiente, porque reciclamos ropa y utilizamos tintas en base a (cuenteo).",
+        "urlSVG": icon1,
+        "altSVG": "Eco-Friendly icon",
+    },
+    {
+        "id": 2,
+        "title": "FORMAS DE PAGO",
+        "content": "Puedes pagar con tu tarjeta de débito/crédito en Webpay o mediante transferencia a nuestra cuenta RUT.",
+        "urlSVG": icon2,
+        "altSVG": "Payment methods icon",
+    },
+    {
+        "id": 3,
+        "title": "SERIGRAFÍA ARTESANAL",
+        "content": "Nuestra serigrafía es totalmente artesanal, no sé, cuenteo sobre el método y las tintas blabla.",
+        "urlSVG": icon3,
+        "altSVG": "Screen Printing Icon",
+    },
+];
