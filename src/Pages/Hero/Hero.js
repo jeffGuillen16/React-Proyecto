da6952fb8d59ecca99aff3833893d1abf20ee10a
Daniela Fernandez Laura <dfernandezl@fi.uba.ar>
2024-01-31 18:51:32 -0300

    Modification in Hero component, adding a products view

--- src/Pages/Hero/Hero.js
+++ src/Pages/Hero/Hero.js
@@ -1,12 +1,18 @@
 import "./Hero.scss";
 import { Container, Col, Row} from 'react-bootstrap';
 import { iconsWithText } from "../../Data/Icons_Data/Icons_data";
+import TitleWithLines from '../../Components/TitleWithLines/TitleWithLines';
+import Product from '../../Components/Product/Product'
+import { Products } from '../../Data/Products_Data/Products_Data';
 
 
 function Hero() {
+    const lastestProducts = Products.slice(-5);
+
     return(
         <>
-            <section className="hero">
+            <div className="hero">
+
                 <Container fluid className="text-center mt-5">
                         
                         <Row className="justify-content-evenly column-gap-5">
@@ -22,8 +28,31 @@ function Hero() {
                                 );
                             })}
                         </Row>
-                    </Container>
-            </section>
+                </Container>
+
+                <section className="products_container">
+                        <TitleWithLines titulo="REVISA NUESTROS ÚLTIMOS PRODUCTOS" />
+                        
+                        <Container fluid className="mt-5">
+                            <Row className={`justify-content-${window.innerWidth >= 992 ? 'between' : 'around'} column-gap-5`}>
+                                {lastestProducts.map(function (product) {
+                                    return(
+                                        <Col className="mb-5" xs={6} md={4} lg={3} xl={2}>
+                                            <Product
+                                                key={product.id}
+                                                urlImage={product.urlImage}
+                                                altImage={product.altImage}
+                                                productName={product.productName}
+                                                price={product.price} />
+                                        </Col>
+                                    );
+                                })}
+                            </Row>
+                        </Container>
+                        
+
+                </section>
+            </div>
         </>
     )
 }
