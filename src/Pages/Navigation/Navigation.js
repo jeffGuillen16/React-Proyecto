c4826a4a7afec93fb4c1e53c845b21d54a4122cf
jeffGuillen16 <marcoguillen97@outlook.com>
2024-03-13 19:29:23 -0300

    add

--- src/Pages/Navigation/Navigation.js
+++ src/Pages/Navigation/Navigation.js
@@ -1,38 +1,29 @@
 import React from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { navigation_data } from "../../Data/Navigation_data";
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { icons } from "../../Data/Icons.js";
 
 import "./Navigation.scss";
+
 function Navigation() {
   const primeraImagen = navigation_data[0][0];
 
   return (
-    <Navbar
-      expand="lg"
-      style={{
-        position: "fixed",
-        zIndex: "900",
-        height: "60px",
-        width: "100%",
-      }}
-    >
+    <Navbar expand="lg" style={{ position: "fixed", zIndex: "900", height: "60px", width: "100%" }}>
       <Container>
         <div className="brand-logo">
-            <img
-              src={primeraImagen.image}
-              alt={`Imagen ${primeraImagen.id}`}
-              width={"65px"}
-              height={"65px"}
-            />
-          
+          <img
+            src={primeraImagen.image}
+            alt={`Imagen ${primeraImagen.id}`}
+            width={"65px"}
+            height={"65px"}
+          />
         </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse>
           <Nav className="my-custom-class">
-            <NavDropdown title="PRODUCTOS" id="basic-nav-dropdown ">
+            <NavDropdown title="PRODUCTOS" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Poleras</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Gorros</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Bolsas</NavDropdown.Item>
@@ -40,48 +31,31 @@ function Navigation() {
               <NavDropdown.Item href="#action/3.4">Tapabocas</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="ILUSTRADORES" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
-                Sonia Maria Cano
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Alfon U Riarte
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">
-                Cristobal Laguna
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Sonia Maria Cano</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Alfon U Riarte</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Cristobal Laguna</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Miguel Angel Ruiz
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Miguel Angel Ruiz</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="COLECIONES" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
-                Colección Amor
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Coleción Navideña
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">
-                Colección Hallowen
-              </NavDropdown.Item>
+            <NavDropdown title="COLECCIONES" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Colección Amor</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Colección Navideña</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Colección Hallowen</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Colección Kawaii
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Colección Kawaii</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link className="navItemsText" href="#link">
-              NOSOTROS
-            </Nav.Link>
+            <Nav.Link className="navItemsText" href="#link">NOSOTROS</Nav.Link>
             <div className="brand-icon">
               <div className="brand-buscador">
-                <a className="icons" href="/">
-                  <FontAwesomeIcon
-                    icon={["fas", icons.MagnifyingGlass]}
-                    size="lg"
-                    className="brand-menu"
-                    style={{ color: "#000", marginTop: "7px" }}
-                  />
-                </a>
+              <a className="icons" href="/">
+                <FontAwesomeIcon
+                  icon={["fas", icons.MagnifyingGlass]}
+                  size="lg"
+                  className="brand-menu"
+                  style={{ color: "#000", marginTop: "7px", backgroundColor: "transparent" }}
+                />
+              </a>
+
               </div>
               <div className="brand-cart">
                 <a className="icons" href="/">
@@ -93,7 +67,7 @@ function Navigation() {
                 </a>
               </div>
               <div className="icons-media">
-                <div className="icon-container">
+              
                   <a className="icons-fab" href="/">
                     <FontAwesomeIcon
                       icon={["fab", icons.Facebook]}
@@ -101,8 +75,8 @@ function Navigation() {
                       style={{ color: "#000" }}
                     />
                   </a>
-                </div>
-                <div className="icon-container">
+              
+              
                   <a className="icons-fab" href="/">
                     <FontAwesomeIcon
                       icon={["fab", icons.Instagram]}
@@ -110,8 +84,8 @@ function Navigation() {
                       style={{ color: "#000" }}
                     />
                   </a>
-                </div>
-                <div className="icon-container">
+              
+              
                   <a className="icons-fab" href="/">
                     <FontAwesomeIcon
                       icon={["fab", icons.Tiktok]}
@@ -120,7 +94,7 @@ function Navigation() {
                     />
                   </a>
                 </div>
-              </div>
+              
             </div>
           </Nav>
         </Navbar.Collapse>
